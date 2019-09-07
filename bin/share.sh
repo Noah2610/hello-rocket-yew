5bc0f7302d9e0c2e16194e9d0ee09931869644fb
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-09-07 10:20:02 +0200

    Working pipeline, serving yew wasm frontend with rocket backend

--- bin/share.sh
+++ bin/share.sh
@@ -4,6 +4,12 @@ _dir="$( dirname "$0" )"
 source "${_dir}/util.sh"
 unset _dir
 
+# https://unix.stackexchange.com/q/32907 (comments)
+# Escape path for use with sed
+function sed_escape {
+  echo "$1" | sed -r 's/([\$\.\*\/\[\\^])/\\\1/g' | sed 's/[]]/\[]]/g'
+}
+
 PROFILE="debug"
 BACKEND="${ROOT}/backend"
 BACKEND_BIN="hello-rocket-yew-backend"
@@ -11,7 +17,11 @@ FRONTEND="${ROOT}/frontend"
 FRONTEND_BIN="hello-rocket-yew-frontend"
 STATIC_BACKEND="${BACKEND}/static"
 STATIC_FRONTEND="${FRONTEND}/static"
-JS_RUNTIME="library-es6"
+JS_RUNTIME="standalone"
+WASM_TARGET="wasm32-unknown-unknown"
+
+cargo_arg_extra=
+[ "$PROFILE" == "release" ] && cargo_arg_extra="--release"
 
 CARGO_ARGS=
 [ "$PROFILE" == "release" ] && CARGO_ARGS="--release"
