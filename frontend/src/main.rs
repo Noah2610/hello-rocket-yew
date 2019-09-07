1c78bc38f2da290424083c0f1d87f2d4deefbaf8
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-09-07 10:27:32 +0200

    Simplify frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,40 +4,27 @@ use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
 struct Model {}
 
-enum Msg {
-    DoIt,
-}
-
 impl Component for Model {
-    // Some details omitted. Explore the examples to see more.
-
-    type Message = Msg;
+    type Message = ();
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
         Model {}
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::DoIt => {
-                // Update your model on events
-                true
-            }
-        }
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
     }
 }
 
 impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
-            // Render your model here
-            <button onclick=|_| Msg::DoIt>{ "Click me!" }</button>
+            <h1>{ "Hello World!" }</h1>
         }
     }
 }
 
 fn main() {
-    // yew::initialize();
     yew::start_app::<Model>();
 }
